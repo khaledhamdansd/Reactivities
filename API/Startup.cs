583854367750c7fcc8ee05bb36877457c04edb09
developer3 <dev3@dev3.com>
2020-06-01 18:40:14 +0400

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,13 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefualtConnection"));
 
             });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",policy=>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
             
         }
@@ -49,6 +56,11 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
 
             app.UseEndpoints(endpoints =>
             {
